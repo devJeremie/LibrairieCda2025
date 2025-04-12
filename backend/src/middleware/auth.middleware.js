bc5f11ae3cbb7c88a667a34088479fc957839cba
devJeremie <chabanaisjeremie@gmail.com>
2025-04-12 12:46:40 +0200

    authMiddlewareStart

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,10 @@
+import jwt from "jsonwebtoken";
+import  User  from "../models/User.js";
+
+const protectRoute = async(req,res,next) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -1,13 +1,42 @@
 import express from "express";
+import cloudinary from "../lib/cloudinary.js";
+import Book from "../models/Books.js";
 
 // Création d'un objet Router pour gérer les routes de l'application
 const router = express.Router();
 
-router.post("/", async (req,res) => {
+router.post("/", protectRoute, async (req,res) => {
     try {
-        
+        // Récupération des données de la requête (titre, légende, note, image)
+        const { title, caption, rating, image} = req.body;
+       
+        // Vérification que tous les champs sont remplis
+       if (!image || !title || !caption || !rating) { 
+        // Retour d'une erreur 400 si un champ est manquant
+        return res.status(400).json({ message: "Veuillez fournir tous les champs"});
+       }
+
+       //Charge les images de cloudinary
+       const uploadResponse = await cloudinary.uploader.upload(image);
+       const imageUrl = uploadResponse.secure_url
+
+       // Création d'un nouveau livre avec les données récupérées
+       const newBook = new Book({
+        title,
+        caption,
+        rating,
+        image: imageUrl,
+        //user: req.user._id,
+       });
+       // Enregistrement du livre en base de données
+       await newBook.save()
+        // Retour du livre créé avec un code de statut 201
+       res.status(201).json(newBook)
+
     } catch (error) {
-        
+        // Gestion des erreurs
+        console.log("Erreur de création du livre", error);
+        res.status(500).json({ message: error.message });
     }
 })
 
