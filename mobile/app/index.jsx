07499744d62d8cff5e3082e8c6a7ffa31eef77c8
devJeremie <chabanaisjeremie@gmail.com>
2025-04-21 18:32:40 +0200

    StartMobile

---
+++ mobile/app/index.jsx
@@ -0,0 +1,23 @@
+import { 
+  Text, View,
+  StyleSheet,
+} from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style ={styles.title}>Welcome to react native CDA</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  }, 
+  title: {
+    color: "blue",
+  }
+})
