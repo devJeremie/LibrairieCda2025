24db3385cc6247e7849504beea2112684e6ec13d
devJeremie <chabanaisjeremie@gmail.com>
2025-05-15 15:28:38 +0200

    TabScreenStart

---
+++ mobile/app/(tabs)/_layout.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import { Tabs } from "expo-router";
+
+export default function Tablayout() {
+  return (
+    <Tabs>
+        <Tabs.Screen name="index"/>
+        <Tabs.Screen name="create"/>
+        <Tabs.Screen name="profile"/>
+    </Tabs>
+  )
+}
