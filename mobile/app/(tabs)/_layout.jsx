0037220a69a58ea760243a87fc6b152a0c5ffbfe
devJeremie <chabanaisjeremie@gmail.com>
2025-05-21 15:29:45 +0200

    ScreenOptionsEnd

--- mobile/app/(tabs)/_layout.jsx
+++ mobile/app/(tabs)/_layout.jsx
@@ -1,29 +1,56 @@
-import { View, Text } from 'react-native'
 import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import COLORS from "../../constants/colors";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Tablayout() {
+  // Utilisation de hook useSafeAreaInsets pour 
+  // récupérer les informations de zone de sécurité de l'écran
+  // La zone de sécurité est la zone de l'écran qui n'est pas
+  //  couverte par les éléments de l'interface utilisateur 
+  // tels que la barre de navigation ou la barre de statut
+  const insets = useSafeAreaInsets();
+
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
+        tabBarActiveTintColor: COLORS.primary,
+        headerTitleStyle: {
+          color: COLORS.textPrimary,
+          fontWeight: "600",
+        },
+        headerShadowVisible: false, 
+
+        tabBarStyle: {
+          backgroundColor: COLORS.background,
+          borderTopWidth: 1,
+          borderTopColor: COLORS.border,
+          paddingTop: 5,
+          paddingBottom: insets.bottom,
+          height: 60 + insets.bottom,
+       },
       }}
     >
         <Tabs.Screen 
           name="index"
           options={{
-            title: "Accueil"
+            title: "Accueil",
+            tabBarIcon: ({color, size}) =>(<Ionicons name="home-outline" size={size} color={color}/>)
           }}
         />
         <Tabs.Screen
           name="create"
           options={{
-            title: "Créer"
+            title: "Créer",
+             tabBarIcon: ({color, size}) =>(<Ionicons name="add-circle-outline" size={size} color={color}/>)
           }}
         />
         <Tabs.Screen 
           name="profile"
           options={{
-            title: "Profil"
+            title: "Profil",
+             tabBarIcon: ({color, size}) =>(<Ionicons name="person-outline" size={size} color={color}/>)
           }}
         />
     </Tabs>
