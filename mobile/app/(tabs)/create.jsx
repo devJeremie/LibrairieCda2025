09995fe8e1dece9a1c703caaa6c9b9798896b63a
devJeremie <chabanaisjeremie@gmail.com>
2025-05-28 08:18:07 +0200

    MajCreate

--- mobile/app/(tabs)/create.jsx
+++ mobile/app/(tabs)/create.jsx
@@ -1,4 +1,5 @@
-import { View, Text } from 'react-native'
+import { View, Text } from 'react-native';
+import { useState } from "react";
 
 export default function Create() {
   const [title, setTitle] = useState("");
